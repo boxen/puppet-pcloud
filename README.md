15c84c775ad2f064676f3c4bf8f6ee2d604f69b7
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-05-31 16:37:25 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# pCloud Puppet Module for Boxen
+
+## Usage
+
+```puppet
+include pcloud
+```
+
+## Required Puppet Modules
+
+* boxen
+
+## Developing
+
+Write code.
+
+Run `script/cibuild`.
---
+++ spec/classes/pcloud_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'pcloud' do
+  it do
+    should contain_package('pCloud').with({
+      :provider => 'pkgdmg',
+      :source   => 'https://c41.pcloud.com/dHZYRhQkZvno1VZZZzsULw7Zc7ZZmb0ZkZI36ZY7wPP18MQYHGh6CMXiHwpRyohj6X/pCloud%20Sync%20-%20Mac%20%28version%201.1.3%29.dmg',
+    })
+  end
+end

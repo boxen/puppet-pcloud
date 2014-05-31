ce731faf8992c76661b837dc5d49a23086e1bd05
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-05-31 17:05:22 +0300

    Install with appdmg instead of pkgdmg

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-# Public: Install "Knock.app" to /Applications.
+# Public: Install "pCloud.app" to /Applications.
 #
 # Examples
 #
@@ -6,6 +6,6 @@
 class pcloud {
   package { 'pCloud':
     source   => 'https://c41.pcloud.com/dHZYRhQkZvno1VZZZzsULw7Zc7ZZmb0ZkZI36ZY7wPP18MQYHGh6CMXiHwpRyohj6X/pCloud%20Sync%20-%20Mac%20%28version%201.1.3%29.dmg',
-    provider => 'pkgdmg'
+    provider => 'appdmg'
   }
 }
--- spec/classes/pcloud_spec.rb
+++ spec/classes/pcloud_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'pcloud' do
   it do
     should contain_package('pCloud').with({
-      :provider => 'pkgdmg',
+      :provider => 'appdmg',
       :source   => 'https://c41.pcloud.com/dHZYRhQkZvno1VZZZzsULw7Zc7ZZmb0ZkZI36ZY7wPP18MQYHGh6CMXiHwpRyohj6X/pCloud%20Sync%20-%20Mac%20%28version%201.1.3%29.dmg',
     })
   end
